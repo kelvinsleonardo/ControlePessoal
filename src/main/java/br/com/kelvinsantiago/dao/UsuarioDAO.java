568d87142d6decd1967197cbe92f2fe19d1dbd75
kelvinsleonardo <kelvinsleonardo@gmail.com>
2015-12-20 16:32:46 -0200

    Alteracoes para funcionamento JPA

--- src/main/java/br/com/kelvinsantiago/dao/UsuarioDAO.java
+++ src/main/java/br/com/kelvinsantiago/dao/UsuarioDAO.java
@@ -2,8 +2,7 @@ package br.com.kelvinsantiago.dao;
 
 import java.util.ArrayList;
 
-import javax.persistence.EntityManager;
-import javax.persistence.TypedQuery;
+import javax.persistence.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -15,39 +14,43 @@ import br.com.kelvinsantiago.model.Usuario;
  * Classe DAO (Data Access Object) responsável por fazer a interqação com a camada de persistencia JPA
  */
 @Repository
+@Transactional
 public class UsuarioDAO {
 
-  @Autowired
-  private EntityManager manager;
+  @PersistenceUnit
+  private EntityManagerFactory factory;
 
-  @Transactional
+  private EntityManager getEntityManager(){
+    return factory.createEntityManager();
+  }
+
   public Boolean adicionar(Usuario usuario) {
-    manager.persist(usuario);
+    getEntityManager().persist(usuario);
     return true;
   }
 
-  @Transactional
   public Boolean editar(Usuario usuario) {
-    manager.merge(usuario);
+    getEntityManager().merge(usuario);
     return true;
   }
 
-  @Transactional
   public Boolean remover(long id) {
-    Usuario usuariobuscado = manager.find(Usuario.class, id);
-    manager.remove(usuariobuscado);
+    Usuario usuariobuscado = getEntityManager().find(Usuario.class, id);
+    getEntityManager().remove(usuariobuscado);
     return true;
   }
 
+    @Transactional(readOnly = true)
   public Usuario buscarPeloCPF(String cpf) {
-    TypedQuery<Usuario> typedQuery = manager.createNamedQuery("Usuario.buscarPeloCPF", Usuario.class);
+    TypedQuery<Usuario> typedQuery = getEntityManager().createNamedQuery("Usuario.buscarPeloCPF", Usuario.class);
     typedQuery.setParameter("cpf", cpf);
     Usuario usuario = typedQuery.getSingleResult();
     return usuario;
   }
 
+    @Transactional(readOnly = true)
   public ArrayList<Usuario> getTodosUsuarios() {
-    TypedQuery<Usuario> typedQuery = manager.createNamedQuery("Usuario.buscarTodosUsuarios", Usuario.class);
+    TypedQuery<Usuario> typedQuery = getEntityManager().createNamedQuery("Usuario.buscarTodosUsuarios", Usuario.class);
     ArrayList<Usuario> usuarios = (ArrayList<Usuario>) typedQuery.getResultList();
     return usuarios;
   }
